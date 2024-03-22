45a73f08c5928fa7b0c75dcb7bd327f4547a2a83
Roblox Hacks creator <87909313+DoraBans@users.noreply.github.com>
2024-03-23 00:00:33 +0100

    Aktualisieren von Cloudflare.lua

--- Cloudflare.lua
+++ Cloudflare.lua
@@ -1,3 +1,12 @@
+local Ui = Instance.new("ScreenGui")
 local frame = Instance.new("Frame") 
 local button = Instance.new("TextButton") 
 local Title = Instance.new("TextLabel")
+local frame2 = Instance.new("Frame")
+
+frame.Parent = Ui
+frame.Size = UDim2.new(0, 200, 0, 100)
+
+Title.Parent = frame
+Title.Size = UDim2.new(1, 0, 0, 25)
+Title.Font = Enum.Font.SourceSans
